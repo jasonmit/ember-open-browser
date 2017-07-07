08c19784eda8d2dc5611272bbfd0470f250c58a4
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-07-06 22:16:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ module.exports = {
     if (options.watcher.serving && process.env.BROWSER !== 'none' && !options.noBrowser) {
       const baseURL = options.rootURL === '' ? '/' : cleanBaseURL(options.rootURL || options.baseURL);
       const url = `http${options.ssl ? 's' : ''}://${options.host || 'localhost'}:${options.port}${baseURL}`;
-
       options.watcher.watcher.once('change', () => opn(url, this._browser));
     }
   }
