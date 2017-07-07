b658673b66160ce941cf4489c60c95e47434cc4c
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-07-06 20:39:57 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,26 +1,7 @@
 # ember-open-browser
 
-This README outlines the details of collaborating on this Ember addon.
+Opens your ember-cli application in the systems default browser during `ember serve`.
 
 ## Installation
 
-* `git clone <repository-url>` this repository
-* `cd ember-open-browser`
-* `npm install`
-
-## Running
-
-* `ember serve`
-* Visit your app at [http://localhost:4200](http://localhost:4200).
-
-## Running Tests
-
-* `npm test` (Runs `ember try:each` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
-
-For more information on using ember-cli, visit [https://ember-cli.com/](https://ember-cli.com/).
+* `ember install ember-open-browser`
--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 /* eslint-env node */
 'use strict';
 
+const opn = require('opn');
+const cleanBaseURL = require('clean-base-url');
+const Watcher = require('ember-cli/lib/models/watcher');
+
 module.exports = {
-  name: 'ember-open-browser'
+  name: 'ember-open-browser',
+  hasOpened: false,
+
+  detectBrowser() {
+    const envBrowser = process.env.BROWSER;
+
+    if (envBrowser && envBrowser !== 'none') {
+      return {
+        app: envBrowser
+      };
+    }
+
+    return {};
+  },
+
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    const _super = Watcher.prototype.didChange;
+    const addon = this;
+
+    Watcher.prototype.didChange = function didChange() {
+      const ret = _super.apply(this, arguments);
+      const { options } = this;
+
+      if (this.serving && !addon.hasOpened && !options.noBrowser) {
+        const baseURL = options.rootURL === '' ? '/' : cleanBaseURL(options.rootURL || options.baseURL);
+        const url = `http${options.ssl ? 's' : ''}://${options.host || 'localhost'}:${options.port}${baseURL}`;
+        opn(url, addon.detectBrowser());
+        addon.hasOpened = true;
+      }
+
+      return ret;
+    }
+  }
 };
