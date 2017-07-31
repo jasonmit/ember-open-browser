0aa383ef2c19eea3dd7b7cd9f5541142cf25bbe5
Jeffrey Cann <cannjeff@gmail.com>
2017-07-31 19:31:58 -0400

    Feature: Reuse existing tab if possible (#4)
    * Feature: Reuse existing tab if possible

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const opn = require('opn');
 const cleanBaseURL = require('clean-base-url');
+const openBrowser = require('./openBrowser');
 
 module.exports = {
   name: 'ember-open-browser',
@@ -26,9 +26,8 @@ module.exports = {
 
     if (options.watcher.serving && this._browser !== 'none' && !options.noBrowser) {
       let uri = options['open-browser-uri'] || this.createUri(options);
-      let opnOptions = this._browser ? { app: this._browser } : {};
 
-      options.watcher.watcher.once('change', () => opn(uri, opnOptions));
+      options.watcher.watcher.once('change', () => openBrowser(uri));
     }
   }
 };
